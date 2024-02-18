75b07c1ee43e9890d35217f1f02bf4d7840b6878
kuro115 <126228724+YuutaMatsuo@users.noreply.github.com>
2024-02-18 18:12:02 +0900

    修正

--- html/index.php
+++ html/index.php
@@ -61,5 +61,46 @@ $comment_array = $pdo->query($sql);
 
 // DBの接続を閉じる
 $pdo = null;
-
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP掲示板</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1 class="title">掲示板アプリ</h1>
+  <hr>
+  <div class="boardWrapper">
+    <section>
+      <?php foreach ($comment_array as $comment) : ?>
+        <article>
+          <div class="wrapper">
+            <div class="nameArea">
+              <span>名前：</span>
+              <p class="username"><?php echo $comment["username"]; ?></p>
+              <time>:<?php echo $comment["postdate"]; ?></time>
+            </div>
+            <p class="comment"><?php echo $comment["comment"]; ?></p>
+          </div>
+        </article>
+      <?php endforeach; ?>
+    </section>
+    <form class="formWrapper" method="POST">
+      <div>
+        <input type="submit" value="書き込む" name="submitButton">
+        <label for="">名前：</label>
+        <input type="text" name="username">
+      </div>
+      <div>
+        <textarea class="commentTextArea" name="comment"></textarea>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
